9e3822cf540ba2a4bdddf551e451e91c7cd9d61e
Eva <evarizm32@gmail.com>
2025-02-09 18:44:06 +0200

    Change the structure in order to deploy it

--- Demo/vite.config.js
+++ Demo/vite.config.js
@@ -3,13 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/IoT-APARS/", // Ensure this matches your GitHub repo name exactly
-  publicDir: "public", // Ensure Vite finds static files
-  build: {
-    outDir: "docs", // GitHub Pages requires build files in docs/
-    emptyOutDir: true, // Clears old files before rebuilding
-    rollupOptions: {
-      input: "public/index.html", // Explicitly tell Vite where to find index.html
-    },
-  },
-});
+  base: "/IoT-APARS/", // Ens
