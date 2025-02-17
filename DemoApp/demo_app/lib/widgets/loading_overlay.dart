1b3f192816beba743baba5fe250cb2501716dd3d
Eva <evarizm32@gmail.com>
2025-02-17 13:01:17 +0200

    Revert "APARS DEMO app implementation"
    This reverts commit 5d2ffbb5ed0b66275ae1fe251dff02b11e9833ca.

---
+++ DemoApp/demo_app/lib/widgets/loading_overlay.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LoadingOverlay extends StatelessWidget {
+  final bool isLoading;
+
+  const LoadingOverlay({super.key, required this.isLoading});
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoading
+        ? Center(
+            child: Container(
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.8),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: CircularProgressIndicator(),
+            ),
+          )
+        : SizedBox.shrink();
+  }
+}
